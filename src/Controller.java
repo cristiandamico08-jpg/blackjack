15ad1372dc14edbd3bb2cdbfc8134461f67b300e
simonenapolitano <simone.napolitano.08@marconigorgonzola.it>
2026-02-14 22:44:32 +0100

    Inserita la generazione di carte casuali

--- src/Controller.java
+++ src/Controller.java
@@ -3,9 +3,12 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.*;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import java.util.Random;
+
 
 public class Controller {
     @FXML
@@ -13,6 +16,8 @@ public class Controller {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    public ImageView myImageView;
 
     public void vaiAScena1(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
@@ -28,6 +33,24 @@ public class Controller {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void pescaCarta(){
+        myImageView.setImage(generaCarta());
+    }
+
+    private Image generaCarta(){
+        Random random = new Random();
+        
+        int numeroN = random.nextInt(13) + 1;
+        String numeroCarta = numeroN == 1 ? "A" : numeroN == 10 ? "T" : numeroN == 11 ? "J" : numeroN == 12 ? "Q" : numeroN == 13 ? "K" : String.valueOf(numeroN);
+        int semeN = random.nextInt(3) + 1;
+        
+        String semeCarta = semeN == 1 ? "S" : semeN == 2 ? "C" : semeN == 3 ? "H" : "D";
+        String fileCarta = "file:assets/img/carte/fronte/" + numeroCarta + semeCarta + "@1x.png";
+        
+        Image carta = new Image(fileCarta);
+        return carta;
     }
 
     public void esci() {
