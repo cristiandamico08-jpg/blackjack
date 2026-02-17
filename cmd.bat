74f9db2d566d361172811c48c3614534f1300736
Cristian Dylan D'Amico <cristian.damico.08@marconigorgonzola.it>
2026-02-17 21:38:51 +0100

    Modifiche file .gitignore

--- cmd.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem imposta il percorso dove hai estratto l’SDK di JavaFX
-set PATH_TO_FX="C:/Program Files/Java/javafx-sdk-25.0.2/lib"
-
-rem avvia l’applicazione passando module‑path e moduli necessari. Utilizza `javaw` al posto di `java` per evitare la finestra del prompt dei comandi
-javaw --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml -jar BlackJack.jar
-
-
-exit
--- creaJAR.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"C:\Program Files\Java\jdk-25.0.2\bin\jar.exe" cfm BlackJack.jar bin/MANIFEST.MF -C bin . -C . assets
