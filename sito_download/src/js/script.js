235f0fca834085a1c0d659389bc5407e3bd43fab
Cristian Dylan D'Amico <cristian.damico.08@marconigorgonzola.it>
2026-03-05 12:04:33 +0100

    Aggiunta funzione di download ai bottoni

--- sito_download/src/js/script.js
+++ sito_download/src/js/script.js
@@ -1,6 +1,23 @@
-function download(){
-    const link = document.createElement('a');
-    link.href = 'https://github.com/cristiandamico08-jpg/blackjack/releases/download/nuovo_setup/setupBlackJack.exe';
-    link.download = 'setupBlackJack.exe';
-    link.click();
+function download(downloadCode){
+    switch(downloadCode){
+        case 1:
+            const link1 = document.createElement('a');
+            link1.href = 'https://github.com/cristiandamico08-jpg/blackjack/releases/download/nuovo_setup/setupBlackJack.exe';
+            link1.download = 'setupBlackJack.exe';
+            link1.click();
+            break;
+        case 2:
+            const link2 = document.createElement('a');
+            link2.href = 'https://github.com/cristiandamico08-jpg/blackjack/releases/download/nuovo_setup/setupBlackJack_ARM64.dmg';
+            link2.download = 'setupBlackJack_ARM64.dmg';
+            link2.click();
+            break;
+        case 3:
+            const link3 = document.createElement('a');
+            link3.href = 'https://github.com/cristiandamico08-jpg/blackjack/releases/download/nuovo_setup/setupBlackJack_x64.dmg';
+            link3.download = 'setupBlackJack_x64.dmg';
+            link3.click();
+            break;
+    }
+    
 }
